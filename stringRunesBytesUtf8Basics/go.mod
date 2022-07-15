57872c9c3f34ab3a159a859acdb34fc7e4153081
amogh2019 <amogh1@outlook.com>
2022-07-15 22:25:25 +0530

    strings basics init

---
+++ stringRunesBytesUtf8Basics/go.mod
@@ -0,0 +1,3 @@
+module stringRunesBytesUtf8Basics
+
+go 1.18
---
+++ stringRunesBytesUtf8Basics/stringBasics.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// backticks doesnt do string processing at all // does not need to escape the double quotes and what not
+	fmt.Println(`hello! "YOLO" \n oye luckky luckky oye!`)
+	
+}
