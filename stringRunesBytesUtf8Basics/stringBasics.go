eb2cc5ba101310ebeab382b0636976532572b984
amogh2019 <amogh1@outlook.com>
2022-07-16 19:29:05 +0530

    stringBasics

--- stringRunesBytesUtf8Basics/stringBasics.go
+++ stringRunesBytesUtf8Basics/stringBasics.go
@@ -6,5 +6,6 @@ func main() {
 
 	// backticks doesnt do string processing at all // does not need to escape the double quotes and what not
 	fmt.Println(`hello! "YOLO" \n oye luckky luckky oye!`)
+
 	
 }
