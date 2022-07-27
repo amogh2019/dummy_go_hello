3a6c042090edafd509ae0c0c0d8dd7a87192caf0
amogh2019 <amogh1@outlook.com>
2022-07-27 11:18:15 +0530

    file basics 2

--- fileIOBasics/fileIOBasics.go
+++ fileIOBasics/fileIOBasics.go
@@ -22,6 +22,54 @@ func main() {
 	if err != nil {
 		// fmt.Println(err)
 		// os.Exit(1)
-		log.Fatal(err) // === print + exit
+		log.Fatal(err) // === print + exit // this also prints a timestamp
 	}
+
+	//CLEAR FILE
+	err = os.Truncate("abc.txt", 0) // shorten file to a size of bytes specified // here 0, means clear the file
+	if err != nil {
+		log.Fatal("error while clearing file", err)
+	}
+
+	//OPEN FILE
+	file1, err := os.Open("abc.txt")
+	if err != nil {
+		log.Fatal("cannot open", err)
+	}
+	file1.Close()
+	file1, _ = os.OpenFile("abc.txt", os.O_APPEND|os.O_CREATE, 0644) // either open in append mode if exists // or create the file if doesnt exist
+	file1.Close()
+
+	// FILE INFO
+	fileInfo, err := os.Stat("abc.txt")
+	fmt.Println(fileInfo)
+	fmt.Println(fileInfo.Name())
+	fmt.Println(fileInfo.Size())
+	fmt.Println(fileInfo.IsDir())
+	fmt.Println(fileInfo.ModTime())
+
+	// fileInfo, err = os.Stat("asdfasfabc.txt")
+	// if err != nil {
+	// 	if os.IsNotExist(err) { // reading the error and taking decision based on error value
+	// 		log.Fatal("this file does not exist ", err)
+	// 	}
+	// }
+
+	// RENAMING
+	oldName := "abc.txt"
+	newName := "abc1.txt"
+	err = os.Rename(oldName, newName)
+
+	if err != nil {
+		log.Fatal("error in renaming ", err)
+	}
+	fmt.Println("renamed")
+
+	// REMOVE
+	err = os.Remove(newName)
+	if err != nil {
+		log.Fatal("error in remoing ", err)
+	}
+	fmt.Println("removed")
+
 }
