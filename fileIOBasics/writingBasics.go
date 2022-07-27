c897308c4c14db0d415ba9b3cfa0a4a3a15bee50
amogh2019 <amogh1@outlook.com>
2022-07-27 17:11:50 +0530

    writing buffered

--- fileIOBasics/writingBasics.go
+++ fileIOBasics/writingBasics.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
@@ -16,7 +18,7 @@ func main() {
 
 	// write by os package // lightweight
 	file, err := os.OpenFile("aa.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	// file, err := os.OpenFile("aa.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644) // this will clear the exiting file
+	// file, err := os.OpenFile("aa.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644) // this will clear the exiting file // WRTIE-ONLY mode?
 	if err != nil {
 		fmt.Println("error in opening file ", err)
 	}
@@ -38,4 +40,30 @@ func main() {
 	if err != nil {
 		fmt.Println("error in writing data to file", file, " ", err)
 	}
+
+	// write by buffered writer
+	// buffered writer maintains a size that holds the data to be written
+	// when the batch is full, it writes the data on the file and reset
+	// we can also manually flush and reset
+	file2, err := os.OpenFile("cc.txt", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
+
+	bufferedWriter := bufio.NewWriterSize(file2, 5)
+	fmt.Println("buffer size", bufferedWriter.Available())
+
+	tempstr := "abcdefghijk"
+	for _, singleChar := range tempstr {
+		fmt.Println([]byte{byte(singleChar)})
+		bufferedWriter.Write([]byte{byte(singleChar)})
+
+		fmt.Println("buff", bufferedWriter.Available())
+		fmt.Println("buff", bufferedWriter.Buffered())
+	}
+	if bufferedWriter.Available() > 0 {
+		bufferedWriter.Flush()
+	}
+
 }
