9fc1ac1c39b915d52adba1196cfc937906756e0e
amogh2019 <amogh1@outlook.com>
2022-07-28 11:20:45 +0530

    buffered reading

--- fileIOBasics/readingBasics.go
+++ fileIOBasics/readingBasics.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	fileName := "go.mod"
-	// READING with io // into buffer // not expecting EOF end of file character in buffer
+	// READING
+	// with io // into buffer // not expecting EOF end of file character in buffer
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -33,17 +36,45 @@ func main() {
 	fullFileInBuffer, _ = io.ReadAll(sameFile)
 	fmt.Println("complete file in buffer created by io package only, from starting point\n", string(fullFileInBuffer))
 
-	// READING with ioutils + manual File management
+	// READING
+	// with ioutils + manual File management
 	sameFile, _ = os.Open(fileName)
 	defer sameFile.Close()
 	fullFileInBuffer, _ = ioutil.ReadAll(sameFile)
 	fmt.Println("complete file in buffer created by io package only, from starting point\n", string(fullFileInBuffer))
 
-	// READING with ioutils + no file management
+	// READING
+	// with ioutils + no file management
 	fileData, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(string(fileData))
+	fmt.Println(strings.Repeat("###", 20))
 
+	// BUFFERED READING
+	// open file // attach a buffered reader to the file
+	// read buffer by buffer till scanner.err != nil   // here if EOF is reached // scanner's err becomes nil and overall scan is false // to let you know that cannot read anymoh
+	f1, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f1.Close()
+
+	buffReader := bufio.NewScanner(f1) // by default buffer breaks the data by '\n' character // you can let the buffer be broken as per your own delimiter
+	// buffReader.Split(bufio.ScanRunes) // delimiter == end of each rune // char by char reading
+	// buffReader.Split(bufio.ScanWords) // delimiter == end of each word // word by word reading
+
+	i := 0
+	for ; buffReader.Scan() != false; i++ { // this scans for new line // returns true if line is read // returns false if EOF or err
+		fmt.Println(i, buffReader.Text())
+		// fmt.Println(i, string(buffReader.Bytes()))
+	}
+	if err := buffReader.Err(); err == nil {
+		fmt.Println("File", fileName, "is read completely, buffered reader scan returned false, and buffered reader error is nil, means it reached EOF")
+	} else if err != nil {
+		log.Fatal("some error in buffered reading", err)
+	}
+
+	fmt.Println(strings.Repeat("###", 20))
 }
