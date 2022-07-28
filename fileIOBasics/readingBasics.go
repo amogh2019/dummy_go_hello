1cd09f437cce2f06b99b7a7f683d9fa0a7cb71ec
amogh2019 <amogh1@outlook.com>
2022-07-28 10:59:32 +0530

    reading with io package

---
+++ fileIOBasics/readingBasics.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+
+	fileName := "go.mod"
+	// READING with io // into buffer // not expecting EOF end of file character in buffer
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	dataSlice := make([]byte, 20)
+	bytesRead, err := io.ReadFull(file, dataSlice)
+	if err != nil {
+		log.Fatal("buffer size is greater than file size, we were expecting to completely fill the buffer with the file data ", err)
+	}
+	fmt.Println("bytes read when buffer size to read is fixed : ", bytesRead, string(dataSlice))
+
+	fullFileInBuffer, err := io.ReadAll(file)
+	fmt.Println("complete file in buffer created by io package only,  continued from last read point in file\n", string(fullFileInBuffer))
+
+	sameFile, _ := os.Open(fileName)
+	fullFileInBuffer, err = io.ReadAll(sameFile)
+	fmt.Println("complete file in buffer created by io package only, from starting point\n", string(fullFileInBuffer))
+
+	// READING with ioutils + manual File management
+	
+
+	// READING with ioutils + no file management
+}
